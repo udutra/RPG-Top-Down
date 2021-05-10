fca04c1cd901b2d0d0414e8143f6c1bf377b9d5a
Guilherme Galarça Dutra <udutra@gmail.com>
2021-05-10 19:41:25 -0300

    Animações

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rig;
-    private Vector2 direction;
+    private Vector2 _direction;
     public float speed;
+    
+    public Vector2 Direction {
+        get {
+            return _direction;
+        }
+        set {
+            _direction = value;
+        }
+    }
 
     private void Start() {
         rig = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-       
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
     private void FixedUpdate() {
-        rig.MovePosition(rig.position + direction * speed * Time.fixedDeltaTime);
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
     }
+
+    
 }
---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    private Player player;
+    private Animator anim;
+
+    private void Start() {
+        player = GetComponent<Player>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if(player.Direction.sqrMagnitude > 0) {
+            anim.SetInteger("Transition", 1);
+        }
+        else {
+            anim.SetInteger("Transition", 0);
+        }
+
+        if(player.Direction.x > 0) {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if(player.Direction.x < 0) {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+}
