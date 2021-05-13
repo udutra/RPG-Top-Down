e988d198e225825e72ba8a42d81bcc825dfd5990
Guilherme Galarça Dutra <udutra@gmail.com>
2021-05-13 16:58:44 -0300

    IA (Parte 2)

--- Assets/Scripts/Npc/NPC.cs
+++ Assets/Scripts/Npc/NPC.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    private int index;
+    public float speed;
+    public List<Transform> paths;
 
+    private void Update() {
+        transform.position = Vector2.MoveTowards(transform.position, paths[index].position, speed * Time.deltaTime);
+        if(Vector2.Distance(transform.position, paths[index].position) < 0.1f ) {
+
+            if(index < paths.Count - 1) {
+                index++;
+            }
+            else {
+                index = 0;
+            }
+        }
+
+        Vector2 direction = paths[index].position - transform.position;
+        if(direction.x > 0) {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if(direction.x < 0) {
+            transform.eulerAngles = new Vector2(0, -180);
+        }
+    }
 }
