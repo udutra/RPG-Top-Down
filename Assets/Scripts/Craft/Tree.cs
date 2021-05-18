dce2c5bb50df5e1dd80178514aba8daccfa6f7c6
Guilherme Galarça Dutra <udutra@gmail.com>
2021-05-17 21:30:28 -0300

    Partículas de folhas

--- Assets/Scripts/Craft/Tree.cs
+++ Assets/Scripts/Craft/Tree.cs
@@ -8,10 +8,12 @@ public class Tree : MonoBehaviour
     [SerializeField] private int totalWood;
     [SerializeField] private Animator anim;
     [SerializeField] private GameObject WoodPrefab;
+    [SerializeField] private ParticleSystem leafs;
 
     public void OnHit() {
         treeHealth--;
         anim.SetTrigger("IsHit");
+        leafs.Play();
 
         if(treeHealth <= 0) {
             for(int i = 0; i < totalWood; i++) {
